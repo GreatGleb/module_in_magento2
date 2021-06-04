1f606ebd29d54951af19cc51cabf064a6b51d665
GreatGleb <44549953+GreatGleb@users.noreply.github.com>
2021-06-04 19:29:43 +0300

    Add files via upload

---
+++ Gleb/Test1/Controller/Block/Name.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Gleb\Test1\Controller\Block;
+
+class Name extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+    {
+        parent::__construct($context);
+    }
+}
---
+++ Gleb/Test1/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Gleb_Test1', __DIR__);
